97ff2fc99a82916090595d331bdf22c6ca9f4154
Ruchini Silva <ruchini.silva@codimite.com>
2026-03-21 11:27:28 +0530

    Task 2: Add assumptions data model

--- js/data/assumptions.js
+++ js/data/assumptions.js
@@ -1,1 +1,11 @@
-// Assumptions Data
+export const defaultAssumptions = {
+  clinicianHourlyRate: 110,
+  itHourlyRate: 65,
+  adminHourlyRate: 35,
+  helpdeskCallTimeMins: 15,
+  helpdeskReduction: 0.70,
+  deviceLifeSpan: 4,
+  hardwareUnitCost: 1200,
+  timeSavedPerLoginSec: 45,
+  dailyLoginsPerUser: 5,
+};
