5b7f9790a935151668823ae82a8edecd59a5f85f
Ruchini Silva <ruchini.silva@codimite.com>
2026-03-21 11:27:38 +0530

    Task 2: Implement core ROI math formulas

--- js/model/formulas.js
+++ js/model/formulas.js
@@ -1,1 +1,15 @@
-// ROI Formulas
+export const calcProductivitySavings = (users, hourlyRate, loginsPerDay, secSavedPerLogin) => {
+  const annualHoursSaved = (users * loginsPerDay * secSavedPerLogin * 260) / 3600;
+  return annualHoursSaved * hourlyRate;
+};
+
+export const calcITOperationalSavings = (devices, itHourlyRate, helpdeskReduction) => {
+  const avgTicketsPerDeviceYear = 4;
+  const hoursPerTicket = 0.5;
+  const totalITCost = devices * avgTicketsPerDeviceYear * hoursPerTicket * itHourlyRate;
+  return totalITCost * helpdeskReduction;
+};
+
+export const calcCapExAvoidance = (devices, unitCost, lifeSpan) => {
+  return (devices * unitCost) / lifeSpan;
+};
